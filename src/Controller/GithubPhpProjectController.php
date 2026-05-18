d8c9dc6e381795bb83dc226cb8c56c8c813cd5f1
julio.vergara <jvca2005@gmail.com>
2026-05-18 16:16:39 -0400

    Adding pagination in the controller

--- src/Controller/GithubPhpProjectController.php
+++ src/Controller/GithubPhpProjectController.php
@@ -4,45 +4,49 @@ namespace App\Controller;
 
 use App\Repository\GithubPhpProjectRepository;
 use App\Service\GithubApiService;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\UX\Turbo\TurboStreamResponse;
 
 #[Route('/github-projects')]
 class GithubPhpProjectController extends AbstractController
 {
     #[Route('', name: 'github_projects_index')]
-    public function index(GithubPhpProjectRepository $repository): Response
+    public function index(GithubPhpProjectRepository $repository, Request $request): Response
     {
-        $projects = $repository->findBy([], ['stars' => 'DESC']);
+        $page = $request->query->getInt('page', 1);
+
+        $qb = $repository->createQueryBuilder('p')
+            ->orderBy('p.stars', 'DESC');
+
+        $adapter = new QueryAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(10);
+        $pagerfanta->setCurrentPage($page);
 
         return $this->render('github_php_project/index.html.twig', [
-            'projects' => $projects,
+            'pagerfanta' => $pagerfanta,
         ]);
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws ClientExceptionInterface
-     */
     #[Route('/refresh', name: 'github_projects_refresh', methods: ['POST'])]
     public function refresh(GithubApiService $service, GithubPhpProjectRepository $repository): TurboStreamResponse
     {
         $service->syncProjects();
 
-        $projects = $repository->findBy([], ['stars' => 'DESC']);
+        $qb = $repository->createQueryBuilder('p')
+            ->orderBy('p.stars', 'DESC');
+
+        $adapter = new QueryAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(10);
 
         $content = $this->renderView('github_php_project/refresh.stream.html.twig', [
-            'projects' => $projects,
+            'pagerfanta' => $pagerfanta,
         ]);
 
         return new TurboStreamResponse($content);
