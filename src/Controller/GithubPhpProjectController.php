d90f1fafaa523cc33fea90f8388dde40c055d571
julio.vergara <jvca2005@gmail.com>
2026-05-27 16:55:55 -0400

    Adding the root route on the GithubPhpProjectController

--- src/Controller/GithubPhpProjectController.php
+++ src/Controller/GithubPhpProjectController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\UX\Turbo\TurboStreamResponse;
 
-#[Route('/github-projects')]
+#[Route('/')]
 class GithubPhpProjectController extends AbstractController
 {
     #[Route('', name: 'github_projects_index')]
